2f0ffa113e1fe1c6ee3d4eec2d4a26d70e43050c
nathansabin <sabinnat001@gmail.com>
2023-07-30 13:12:12 -0700

    staged webpack config, attempted to set up install button and service workers

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # text-editor-PWA
+
+## TODO files:
+3. ! ./client/src-sw.js -- impliment service works to cache 
+4. ! ./client/src/js/install.js -- set up handlers
+
+### Should be DONE
+1. ? ./client/webpack.config.js -- configure loaders, babel, plugins, service workers
+2. ? ./client/src/js/index.js -- check for service workers
+3. ? ./client/src/js/database.js -- insert data
+
+### DONE
+1. V ./package.json -- set up "scripts" 
+
+### legand
+? = should be done
+V = DONE
+! = TODO
---
+++ client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+// opens db and adds content
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+
+  const text = db.transaction("jate", "readwrite");
+  const store = text.objectStore("jate");
+  const request = store.add({text: content});
+  const result = await request;
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const text = db.transaction("jate", "readonly");
+  const store = text.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+
+  console.log(result);
+}
+
+initdb();
